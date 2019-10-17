8f1fffe359a913bce54eaed822e5a38673ebd945
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-10-17 11:40:52 +0200

    Rewrite caurosel
    Change-Id: I71ef0ff8e7a5a0c8c29e3d4ec9d2666878db09bd

--- blocks/caurosel.js
+++ blocks/caurosel.js
@@ -15,130 +15,150 @@ wp.blocks.registerBlockType('wmcz/caurosel', {
     save: () => {
         return null;
     },
-    edit: ( props ) => {
-        function onChangeHeadline( value ) {
-            props.setAttributes( {
-                headline: [
-                    value
-                ]
-            } );
-        }
-
-        function onChangeDescription( value ) {
-            props.setAttributes( {
-                description: [
-                    value
-                ]
-            } );
-        }
-
-
-        function generateSlide( headline, description ) {
-            return React.createElement(
-                'div',
-                {
-                    class: "wmcz-caurosel-container"
-                },
-                React.createElement(
+    edit: (props) => {
+        class GenerateSlide extends React.Component {
+            render() {
+                return (React.createElement(
                     'div',
                     {
-                        class: "wmcz-caurosel-left"
-                    }
-                ),
-                React.createElement(
-                    'div',
-                    {
-                        class: "wmcz-caurosel-right-colored"
-                    },
-                    el(
-                        RichText,
-                        {
-                            tagName: 'h2',
-                            className: 'wmcz-caurosel-headline',
-                            onChange: onChangeHeadline,
-                            value: headline
-                        }
-                    ),
-                    el(
-                        RichText,
-                        {
-                            tagName: 'p',
-                            className: 'wmcz-caurosel-description',
-                            onChange: onChangeDescription,
-                            value: description
-                        }
-                    )
-                )
-            );
-        }
-
-        function getSlide( index ) {
-            return generateSlide( props.attributes.headline[index], props.attributes.description[index] );
-        }
-
-        let screenControls = [];
-        for (let i = 0; i < props.attributes.headline.length; i++) {
-            screenControls.push(React.createElement(
-                'a' ,
-                {
-                    class: "wmcz-caurosel-screen-control",
-                    href: "#",
-                    onClick: () => {
-                        console.log('not implemented');
-                        
-                    }
-                },
-                i+1
-            ));
-        }
-
-        return React.createElement(
-            'div',
-            {
-                class: 'wmcz-caurosel-admin-container'
-            },
-            React.createElement(
-                'div',
-                {
-                    class: "wmcz-caurosel-controls"
-                },
-                React.createElement(
-                    'div',
-                    {
-                        class: "wmcz-caurosel-left"
+                        class: "wmcz-caurosel-container"
                     },
                     React.createElement(
                         'div',
                         {
-                            class: "wmcz-caurosel-screen-controls"
-                        },
-                        screenControls
-                    )
-                ),
-                React.createElement(
-                    'div',
-                    {
-                        class: "wmcz-caurosel-right"
-                    },
+                            class: "wmcz-caurosel-left"
+                        }
+                    ),
                     React.createElement(
-                        'button',
+                        'div',
                         {
-                            class: 'wmcz-caurosel-new-slide',
+                            class: "wmcz-caurosel-right-colored"
+                        },
+                        el(
+                            RichText,
+                            {
+                                tagName: 'h2',
+                                className: 'wmcz-caurosel-headline',
+                                onChange: (value) => {
+                                    let arrayNew = props.attributes.headline.slice(); // HACK
+                                    arrayNew[this.props.index] = value;
+                                    props.setAttributes({
+                                        headline: arrayNew
+                                    });
+                                },
+                                value: this.props.headline
+                            }
+                        ),
+                        el(
+                            RichText,
+                            {
+                                tagName: 'p',
+                                className: 'wmcz-caurosel-description',
+                                onChange: (value) => {
+                                    let arrayNew = props.attributes.description.slice(); // HACK
+                                    arrayNew[this.props.index] = value;
+                                    props.setAttributes({
+                                        description: arrayNew
+                                    });
+                                },
+                                value: this.props.description
+                            }
+                        )
+                    )
+                ))
+            }
+        }
+
+        class Carousel extends React.Component {
+            render() {
+                let screenControls = [];
+                for (let i = 0; i < props.attributes.headline.length; i++) {
+                    screenControls.push(React.createElement(
+                        'a',
+                        {
+                            class: "wmcz-caurosel-screen-control",
+                            href: "#",
                             onClick: () => {
                                 console.log('not implemented');
+                                this.props.index = i;
                             }
                         },
-                        'Nová obrazovka'
+                        i + 1
+                    ));
+                }
+
+                return (
+                    React.createElement(
+                        'div',
+                        {
+                            class: 'wmcz-caurosel-admin-container'
+                        },
+                        React.createElement(
+                            'div',
+                            {
+                                class: "wmcz-caurosel-controls"
+                            },
+                            React.createElement(
+                                'div',
+                                {
+                                    class: "wmcz-caurosel-left"
+                                },
+                                React.createElement(
+                                    'div',
+                                    {
+                                        class: "wmcz-caurosel-screen-controls"
+                                    },
+                                    screenControls
+                                )
+                            ),
+                            React.createElement(
+                                'div',
+                                {
+                                    class: "wmcz-caurosel-right"
+                                },
+                                React.createElement(
+                                    'button',
+                                    {
+                                        class: 'wmcz-caurosel-new-slide',
+                                        onClick: () => {
+                                            props.attributes.headline.push('')
+                                            props.attributes.description.push('')
+                                            this.props.index = props.attributes.headline.length - 1;
+                                            this.props.headline = props.attributes.headline[this.props.index];
+                                            this.props.description = props.attributes.description[this.props.index];
+                                            this.forceUpdate()
+                                        }
+                                    },
+                                    'Nová obrazovka'
+                                )
+                            )
+                        ),
+                        React.createElement(
+                            'div',
+                            {
+                                class: "wmcz-caurosel-outer-container"
+                            },
+                            React.createElement(
+                                GenerateSlide, {
+                                description: this.props.description,
+                                headline: this.props.headline,
+                                index: this.props.index
+                            },
+                                null
+                            )
+                        )
                     )
-                )
-            ),
-            React.createElement(
-                'div',
-                {
-                    class: "wmcz-caurosel-outer-container"
-                },
-                getSlide(0)
-            )
-        );
+                );
+            }
+        }
+
+        return React.createElement(
+            Carousel, {
+            headline: props.attributes.headline[props.attributes.headline.length - 1],
+            description: props.attributes.description[props.attributes.headline.length - 1],
+            index: props.attributes.headline.length - 1
+        },
+            null
+        )
     }
 });
