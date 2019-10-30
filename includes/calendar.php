55b2e43f631487e2091e780f59e9fe9636c03d3e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-10-30 01:07:37 +0100

    Dynamize map
    Change-Id: Ic1e0f4cc46ff9c11926e111405f17d087354168b

--- includes/calendar.php
+++ includes/calendar.php
@@ -1,15 +1,18 @@
 <?php
 
 use ICal\ICal;
+use maxh\Nominatim\Nominatim;
 
 require_once 'place.php';
 
 class WmczCalendar {
     protected $ical;
+    protected $url;
     protected $maxEvents;
 
    public function __construct($url, $maxEvents = null)
     {
+        $this->url = $url;
         $this->ical = new ICal( $url );
         $this->maxEvents = $maxEvents;
     }
@@ -34,10 +37,39 @@ class WmczCalendar {
         return $res;
     }
 
+    protected function addressToPlace( $address ) {
+        $nominatim = new Nominatim( "https://nominatim.openstreetmap.org" );
+        $search = $nominatim->newSearch();
+        $addressExploded = explode( ', ', $address );
+        $search->query( implode( ', ', array_slice( $addressExploded, 0, 2 ) ) ); // HACK
+        $result = $nominatim->find($search);
+        return new Place( $result[0]["lat"], $result[0]["lon"] );
+    }
+
+    public function getPlacesFetch() {
+        $places = [];
+        $from = new DateTime();
+        $to = new DateTime('+1 months');
+        $events = $this->ical->eventsFromRange(
+            $from->format('Y-m-d'),
+            $to->format('Y-m-d')
+        );
+        foreach ( $events as $event ) {
+            if ( $event->location ) {
+                $places[] = $this->addressToPlace( $event->location );
+            }
+        }
+        return $places;
+    }
+
     public function getPlaces() {
-        return [
-            new Place(50.03861, 15.77916)
-        ];
+        $file = dirname( __FILE__ ) .  '/../data/calendar-places-' . hash( "md5", $this->url ) . '.json';
+        if ( !file_exists( $file ) ) {
+            $places = $this->getPlacesFetch();
+            file_put_contents( $file, json_encode( $places ) );
+            return $places;
+        }
+        return json_decode( file_get_contents( $file ) );
     }
 
     public function getEventsNow() {
