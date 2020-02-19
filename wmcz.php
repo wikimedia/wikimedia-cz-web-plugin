87de67231a1202afe63c7043141272dbe1e219a6
Martin Urbanec <martin@urbanec.cz>
2020-02-19 09:56:56 +0100

    Use capital letters in filenames
    Change-Id: Iea98aa113ae21496eced8bd4efb4db6c4cc2c858

--- wmcz.php
+++ wmcz.php
@@ -9,12 +9,20 @@
 **/
 
 spl_autoload_register(function ($class) {
-	$filename = str_replace( 'wmcz', '', strtolower($class) );
+	$filename = str_replace( 'wmcz', '', $class );
 	$path = dirname( __FILE__ ) . '/includes/' . $filename . '.php';
 
 	if ( file_exists( $path ) ) {
 		include $path;
 		return true;
+	} else {
+		$filename = str_replace( 'wmcz', '', strtolower($class) );
+		$path = dirname( __FILE__ ) . '/includes/' . $filename . '.php';
+
+		if ( file_exists( $path ) ) {
+			include $path;
+			return true;
+		}
 	}
 
 	return false;
