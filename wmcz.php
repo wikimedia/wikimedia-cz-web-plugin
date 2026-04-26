66409679ccb453a771507e0ca3dece4c8b30323f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-04-26 22:49:00 +0200

    Fix Undefined array key #3
    Change-Id: I43f121250bc484418587dcd58e2f560676f6ceed

--- wmcz.php
+++ wmcz.php
@@ -453,7 +453,7 @@ function wmcz_block_latest_news_render_callback( $attributes ) {
     }
     $result .= get_the_posts_navigation();
     $result .= '</div>';
-    if ($q->found_posts > $maxNews && $maxNews != 0 && $attributes['tag'] != null && $attributes['tag'] != '') {
+    if ($q->found_posts > $maxNews && $maxNews != 0 && array_key_exists( 'tag', $attributes ) && $attributes['tag'] != null && $attributes['tag'] != '') {
         $tag = get_term_by( 'slug', $attributes['tag'], 'post_tag' );
         $result .= '<a href="' . get_tag_link( $tag->term_id ) . '">' . __( 'All news', 'wmcz-plugin' ) . '</a>';
     }
